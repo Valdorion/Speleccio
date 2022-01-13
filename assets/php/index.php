dbaa9a5b370ef7208ad3e0f173d2c87c3bc7152e
Valdorion <70708301+Valdorion@users.noreply.github.com>
2022-01-13 10:17:36 +0100

    Add files via upload

---
+++ assets/php/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>
+            Speleccio
+        </title>
+        <link rel="stylesheet" href="css.css"/>
+    </head>
+
+    <body>
+        <header id="header">
+            <div class="site-brand">
+                <h1>
+                    Titre
+                </h1>
+            </div>
+        </header>
+        <main>
+            <section>
+                <img src="navbar.png" width="2048px" height="auto"/>
+                <div>
+                    <object class="dynmap" frameborder="0" data="http://51.77.169.168:25567/?worldname=world&mapname=flat&zoom=0&x=0&y=64&z=0#" width="100%" height="500px"></object>
+                </div>
+            </section>
+            <section>
+                <div>
+                </div>
+            </section>
+            <section>
+                <div>
+                </div>
+            </section>
+        </main>
+        <footer>
+            
+        </footer>
+    </body>
+</html>
