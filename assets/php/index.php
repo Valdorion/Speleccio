3fbcc1835f78c2dc02bf04e3a3c39fbc324e060e
Valdorion <valdorn2@gmail.com>
2022-01-18 08:48:03 +0100

    update

--- assets/php/index.php
+++ assets/php/index.php
@@ -11,16 +11,39 @@
     <body>
         <header id="header">
             <div class="site-brand">
-                <h1>
-                    Titre
-                </h1>
+                <img src="../src/img/logo-site.png" alt="Logo de Speleccio">
             </div>
+            <nav class="navbar" role="navigation">
+                <ul>
+                    <li>
+                        <a href="index.php">
+                            <span>
+                                Speleccio
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="membres.php">
+                            <span>
+                                 Les Membres
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="projects.php">
+                            <span>
+                                Les Projets
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
         </header>
         <main>
             <section>
-                <img src="navbar.png" width="2048px" height="auto"/>
+                <img src="navbar.png" width="256px" height="auto"/>
                 <div>
-                    <object class="dynmap" frameborder="0" data="http://51.77.169.168:25567/?worldname=world&mapname=flat&zoom=0&x=0&y=64&z=0#" width="100%" height="500px"></object>
+                    <object class="dynmap" frameborder="0" data="http://51.77.169.168:25567/?worldname=world&mapname=flat&zoom=75&x=801&y=64&z=-467#" width="100%" height="500px"></object>
                 </div>
             </section>
             <section>
