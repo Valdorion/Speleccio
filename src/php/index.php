983d9b636b66e144c6b42f620369fc7a55e39ef6
Valdorion <valdorn2@gmail.com>
2022-02-09 16:57:11 +0100

    update rangement et scss

---
+++ src/php/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>
+            Speleccio
+        </title>
+        <link rel="stylesheet" href="css.css"/>
+    </head>
+
+    <body style="margin: 0; width: 100%;">
+        <header id="header">
+            <div class="site-brand" style="width: 100%; height: auto;">
+                <img src="../../src/img/site-band.png" alt="Logo de Speleccio" style="width: 100%; height: 5rem;">
+            </div>
+            <nav class="navbar" role="navigation">
+                <ul>
+                    <li>
+                        <a href="news.php">
+                            <span>
+                                Les News
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="wiki.php">
+                            <span>
+                                 Le Wiki
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="projects.php">
+                            <span>
+                                Les Projets
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="login.php">
+                            Se Connecter
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <main id="main">
+            <section id="presentation">
+                <div class="description">
+                </div>
+                <div class="trailer">
+                </div>
+            </section>
+            <section id="event-and-news">
+                <div class="carousel">
+                </div>
+            </section>
+            <section id="diffusions">
+                <div class="card diffusions">
+                    <div class="lives">
+                    </div>
+                    <div class="videos">
+                    </div>
+                </div>
+            </section>
+        </main>
+        <footer>
+            
+        </footer>
+    </body>
+</html>
+
+
+
+<!--<object class="dynmap" frameborder="0" data="http://51.77.169.168:25567/?worldname=world&mapname=surface&zoom=4&x=740&y=64&z=-590#" width="100%" height="500px"></object>-->
